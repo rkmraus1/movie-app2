1443c95451befefeb240dfaaa6f9f1e1d8c9670e
rkmraus1 <rkmrdse@gmail.com>
2025-08-04 14:07:13 +0900

    映画マイリスト追加実装完了

---
+++ src/hooks/useMyList.ts
@@ -0,0 +1,37 @@
+// /現在のマイリストの状態を保持・同期
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import type { Movie } from "../types/media";
+
+export const useMyList = (userId: string | null) => {
+  const [myList, setMyList] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMyList = async () => {
+      if (!userId) {
+        setMyList([]);// ログアウト時は空に
+        return;
+      }
+
+      const myListRef = collection(db, "users", userId, "mylist");
+      const snapshot = await getDocs(myListRef);
+      const list: Movie[] = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          id: data.id,
+          original_title: data.original_title,
+          overview: data.overview,
+          poster_path: data.poster_path,
+          release_date: data.release_date,
+        };
+      });
+
+      setMyList(list);
+    };
+
+    fetchMyList();
+  }, [userId]);
+
+  return { myList };
+};
